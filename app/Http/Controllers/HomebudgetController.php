65e304d54a8aa582b9044f8c92464d5e52f374c8
Haya0502 <100024971+Haya0502@users.noreply.github.com>
2024-02-12 21:25:30 +0900

    First commit

---
+++ app/Http/Controllers/HomebudgetController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\HomeBudget;
+
+class HomebudgetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $homebudgets = HomeBudget::with('category')->orderBy('date', 'desc')->paginate(5);
+        $income = HomeBudget::where('category_id', 6)->sum('price');
+        $payment = HomeBudget::where('category_id', '!=', 6)->sum('price');
+        return view('homebudget.index', compact('homebudgets', 'income', 'payment'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'date' => 'required|date',
+            'category' => 'required|numeric',
+            'price' => 'required|numeric'
+        ]);
+
+        $result = HomeBudget::create([
+            'date' => $request->date,
+            'category_id' => $request->category,
+            'price' => $request->price,
+        ]);
+
+        if(!empty($result)) {
+            session()->flash('flash_message', '読み込み成功');
+        }else {
+            session()->flash('flash_error_message', '読み込み失敗');
+        }
+
+        // ブログポストは有効
+        return redirect('/');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $homebudget = HomeBudget::find($id);
+        return view('homebudget.edit', compact('homebudget'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $validated = $request->validate([
+            'date' => 'required|date',
+            'category' => 'required|numeric',
+            'price' => 'required|numeric'
+        ]);
+
+        $hasData = HomeBudget::where('id', '=', $request->id);
+        if($hasData->exists()) {
+            $hasData->update([
+                'date' => $request->date,
+                'category_id' => $request->category,
+                'price' => $request->price
+            ]);
+            session()->flash('flash_message', '更新成功');
+        }else {
+            session()->flash('flash_error_message', '更新失敗');
+        }
+
+        // ブログポストは有効
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $homebudget = HomeBudget::find($id);
+        $homebudget->delete();
+        session()->flash('flash_message', '削除成功');
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomebudgetController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('homebudget.index');
+});
+
+Route::get('/',[HomebudgetController::class, 'index'])->name('index');
+Route::post('/post',[HomebudgetController::class, 'store'])->name('store');
+Route::get('/edit/{id}',[HomebudgetController::class, 'edit'])->name('homebudget.edit');
+Route::put('/update',[HomebudgetController::class, 'update'])->name('homebudget.update');
+Route::post('/destroy/{id}',[HomebudgetController::class, 'destroy'])->name('homebudget.destroy');
